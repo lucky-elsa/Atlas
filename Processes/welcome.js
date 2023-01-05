6c3dd880c1bffe5513ac5367b3955d804e406889
Shubha Pratim Biswas <shubhapratimbiswas@gmail.com>
2023-01-06 00:47:40 +0530

    Created Group Welcome / Leave messages

---
+++ Processes/welcome.js
@@ -0,0 +1,51 @@
+module.exports = async (Miku, anu) => {
+  try {
+    let metadata = await kiku.groupMetadata(anu.id);
+    let participants = m.participants;
+
+    for (let num of participants) {
+      try {
+        ppuser = await Miku.profilePictureUrl(num, "image");
+      } catch {
+        ppuser = "https://wallpapercave.com/wp/wp10753770.jpg";
+      }
+
+      if (anu.action == "add") {
+        let WAuserName = num;
+        mikutext = `
+Hello @${WAuserName.split("@")[0]},
+I am *Miku Nakano*, Welcome to ${metadata.subject}.
+*Group Description:*
+${metadata.desc}
+`;
+
+        let buttonMessage = {
+          image: await getBuffer(ppgroup),
+          mentions: [num],
+          caption: mikutext,
+          footer: `${global.BotName}`,
+          headerType: 4,
+        };
+      } else if (anu.action == "remove") {
+        let WAuserName = num;
+        mikutext = `
+Sayonara 👋, @${WAuserName.split("@")[0]},
+I hope you will come back soon, but we are not going to miss you though!
+`;
+
+        let buttonMessage = {
+          image: await getBuffer(ppuser),
+          mentions: [num],
+          caption: mikutext,
+          footer: `${global.BotName}`,
+          headerType: 4,
+        };
+      }
+      Miku.sendMessage(anu.id, buttonMessage)
+    }
+				
+
+  } catch (err) {
+    console.log(err);
+  }
+};
