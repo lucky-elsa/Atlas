881729b765fc65c5cf5d20f022a927af9cd7ffc3
Fantox <shubhapratimbiswas@gmail.com>
2023-01-12 12:34:07 +0530

    Beta Push by @FantoX001

---
+++ Commands/Group/demote.js
@@ -0,0 +1,40 @@
+require("../../config.js");
+require("../../core.js");
+
+module.exports = {
+  name: "demote",
+  alias: ["dem"],
+  desc: "Demote a member",
+  category: "Group",
+  usage: "[@user]",
+  start: async (
+    Miku,
+    m,
+    { text, prefix, isBotAdmin, isAdmin, mentionByTag,pushName}
+  ) => {
+    if (!text)
+      return Miku.sendMessage(
+        m.from,
+        { text: `Please tag a user to promote!` },
+        { quoted: m }
+      );
+    if (!isAdmin)
+      return Miku.sendMessage(m.from, { text: mess.useradmin }, { quoted: m });
+
+    const mentionedUser = mentionByTag;
+    let users = (await mentionedUser[0]) || m.msg.contextInfo.participant;
+
+    try {
+      await Miku.groupParticipantsUpdate(m.from, [users], "demote").then(
+        (res) =>
+          Miku.sendMessage(
+            m.from,
+            { text: `User has been *Demoted* Successfully by *${pushName}*` },
+            { quoted: m }
+          )
+      );
+    } catch (err) {
+      Miku.sendMessage(m.from, { text: `${mess.botadmin}` }, { quoted: m });
+    }
+  },
+};
--- Commands/Group/promote.js
+++ Commands/Group/promote.js
@@ -1,21 +1,40 @@
-require('../../config.js');
-require('../../core.js');
+require("../../config.js");
+require("../../core.js");
 
 module.exports = {
-    name: "promote",
-    alias: ["prom"],
-    desc: "Promote a member",
-    category: "Group",
-    usage: "[@user]",
-    start: async(Miku, m, { text, prefix, isBotAdmin,isAdmin,mentionByTag}) => {
-        if(!text) return Miku.sendMessage(m.from,{text:`Please provide a user to promote!`},{quoted:m})
-        if (!isAdmin) return Miku.sendMessage(m.from, { text: mess.useradmin }, { quoted: m })
-        if (!isBotAdmin) return Miku.sendMessage(m.from, { text: mess.botadmin }, { quoted: m })
-        
-        const mentionedUser= mentionByTag
-        let users = await (mentionedUser[0]) || m.msg.contextInfo.participant
+  name: "promote",
+  alias: ["prom"],
+  desc: "Promote a member",
+  category: "Group",
+  usage: "[@user]",
+  start: async (
+    Miku,
+    m,
+    { text, prefix, isBotAdmin, isAdmin, mentionByTag,pushName,}
+  ) => {
+    if (!text)
+      return Miku.sendMessage(
+        m.from,
+        { text: `Please tag a user to promote!` },
+        { quoted: m }
+      );
+    if (!isAdmin)
+      return Miku.sendMessage(m.from, { text: mess.useradmin }, { quoted: m });
 
-        await Miku.groupParticipantsUpdate(m.from, [users], "promote").then((res) =>  
-        Miku.sendMessage(m.from,{text:`User has been promoted Successfully!`},{quoted:m}))
+    const mentionedUser = mentionByTag;
+    let users = (await mentionedUser[0]) || m.msg.contextInfo.participant;
 
-}}
+    try {
+      await Miku.groupParticipantsUpdate(m.from, [users], "promote").then(
+        (res) =>
+          Miku.sendMessage(
+            m.from,
+            { text: `User has been *Promoted* Successfully! by *${pushName}*` },
+            { quoted: m }
+          )
+      );
+    } catch (err) {
+      Miku.sendMessage(m.from, { text: `${mess.botadmin}` }, { quoted: m });
+    }
+  },
+};
