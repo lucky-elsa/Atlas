330281d29019dde62d89ad8e54714e63343a3b18
Devime <121723330+Devime69@users.noreply.github.com>
2023-01-14 18:06:54 +0530

    Update love.js

--- Commands/Fun/love.js
+++ Commands/Fun/love.js
@@ -1,11 +1,29 @@
 module.exports = {
-    name: "iloveumiku",
-    alias: ["loveu"],
-    desc: "proposed",
-    cool:3,
-    react: "❤",
-    category: "fun",
-    start: async(Miku, m,{pushName}) => {
-        await Miku.sendMessage(m.from,{text:`I love you too *${pushName}*`},{quoted:m})
-    }
-}
+  name: "loveu",
+  alias: ["iloveumiku"],
+  desc: "what do u want",
+  cool: 3,
+  react: "🫣",
+  category: "fun",
+  start: async (Miku, m, { pushName, prefix }) => {
+    let buttons = [
+      {
+        buttonId: `${prefix}hmiku`,
+        buttonText: { displayText: "Mʏ ʜᴇɴᴛᴀɪ ᴘɪᴄs😜🤭" },
+        type: 1,
+      },
+      
+    ];
+
+    let buttonMessage = {
+      image: { url: "https://i.ibb.co/dfQh0K5/IMG-20230114-155829.jpg" },
+      caption: `Oʜ ʀᴇᴀʟʟʏ!! I Lᴏᴠᴇ Yᴏᴜ Tᴏᴏ *${pushName}* Sᴇɴᴘᴀɪ💖❣️
+Dᴏ Yᴏᴜ Wᴀɴɴᴀ Sᴇᴇ Mʏ Hᴇɴᴛᴀɪ Pɪᴄs *${pushName}* Sᴇɴᴘᴀɪ 🫣 ?\n`,
+      footer: `*ᴍɪᴋᴜ ɴᴀᴋᴀɴᴏ*`,
+      buttons: buttons,
+      headerType: 4,
+    };
+
+    await Miku.sendMessage(m.from, buttonMessage, { quoted: m });
+  },
+};
