865c4c6e6c4289b14be724bfa387774a3c924265
Kai... !! <76661641+Kai0071@users.noreply.github.com>
2023-01-15 20:21:22 +0530

    @Kai0071

--- Commands/Reactions/dance.js
+++ Commands/Reactions/dance.js
@@ -15,7 +15,7 @@ module.exports = {
   desc: "To dance for user",
   category: "Reaction",
   usage: `dance @user`,
-  react: "🍁",
+  react: "😯",
   start: async (Miku, m, { text, prefix, args,mentionByTag }) => {
     var pat = await fetchJson(`https://api.waifu.pics/sfw/dance`);
     try {
