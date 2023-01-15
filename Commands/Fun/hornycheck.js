d283f396ee7fad58802f33ea5658869e15b55aa2
Devime <121723330+Devime69@users.noreply.github.com>
2023-01-16 03:20:22 +0530

    Update hornycheck.js

--- Commands/Fun/hornycheck.js
+++ Commands/Fun/hornycheck.js
@@ -21,7 +21,7 @@ module.exports = {
     
     let mikupic ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCHmi6Dyk_MuDRZRNTC1-PWQoUS9khIckXMQ&usqp=CAU';
 
-    let Mikutext = `Horny Check : @${mentionedUser.split("@")[0]}\n\nAnswer : *${dey}%*🤣`
+    let Mikutext = `Horny Check Of : @${mentionedUser.split("@")[0]}\n\nAnswer : *${dey}%*🤣`
 
     Miku.sendMessage(m.from, { image: {url: mikupic},caption: Mikutext , mentions: [mentionedUser]}, { quoted: m });
 
