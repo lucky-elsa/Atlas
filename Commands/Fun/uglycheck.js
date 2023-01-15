ec62bf695a5ffe5a9babcdcb348bbf502bfb05fa
Devime <121723330+Devime69@users.noreply.github.com>
2023-01-16 02:01:25 +0530

    Update uglycheck.js

--- Commands/Fun/uglycheck.js
+++ Commands/Fun/uglycheck.js
@@ -21,7 +21,7 @@ module.exports = {
     
     let mikupic ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCHmi6Dyk_MuDRZRNTC1-PWQoUS9khIckXMQ&usqp=CAU';
 
-    let Mikutext = `Ugly Check : @${mentionedUser.split("@")[0]}\n\nAnswer : *${dey}*`
+    let Mikutext = `Ugly Check : @${mentionedUser.split("@")[0]}\n\nAnswer : *${dey}%*😂`
 
     Miku.sendMessage(m.from, { image: {url: mikupic},caption: Mikutext , mentions: [mentionedUser]}, { quoted: m });
 
