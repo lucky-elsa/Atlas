3087ef4f72b3be2a9d1d172f48d18a8c590a4074
kirito2355 <119128285+kirito2355@users.noreply.github.com>
2023-01-20 20:13:04 +0500

    Add files via upload

---
+++ Commands/Mods/modlist.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+require("../../config.js");
+require("../../Core.js");
+const { mku } = require("../../lib/dataschema.js");
+
+module.exports = { 
+
+    name: "modlist", 
+    desc: "To view the list of current Mods", 
+    alias: ["listmod"],
+    category: "Mods", 
+    usage: "modlist", 
+    react: "🔨", 
+    start: async (
+      Miku, 
+      m, 
+      { text, prefix, mentionByTag, pushName, isCreator,owner,includes} 
+    ) => { 
+      var modStatus = await mku.findOne({id:m.sender}).then(async (user) => {
+        if (user.addedMods=="true") {
+          return "true";
+        }
+        else{
+          return "false";
+        }
+      }).catch(error => {
+        console.log(error)
+        //return Miku.sendMessage(m.from, { text: `An internal error occurred while checking your mod status.` }, { quoted: m });
+      });
+
+
+      if (!isCreator&&!modStatus=="true") 
+        return Miku.sendMessage(m.from, { text: 'Sorry, only my *Devs* and *Mods* can use this command !' }, { quoted: m });
+
+        try { 
+        
+            var modlist = await mku.find({addedMods: "true"});
+            var modlistString = "";
+            modlist.forEach(mod => {
+              modlistString += `\n@${mod.id.split("@")[0]}\n`
+            });
+            var mention = modlist.map(mod => mod.id);
+            if(modlistString == "") modlistString = "No Mods found.";
+            return Miku.sendMessage( 
+              m.from, 
+              { text: `Current Mods: \n${modlistString}`, mentions: mention }, 
+              { quoted: m } 
+            );
+          } catch (err) { 
+            console.log(err);
+            return Miku.sendMessage(m.from, { text: `An internal error occurred while fetching the mod list.` }, { quoted: m });
+          } 
+        }, 
+    }
