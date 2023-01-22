02b59481a1d31ea62e924270289b45289035219c
kirito2355 <119128285+kirito2355@users.noreply.github.com>
2023-01-22 20:45:09 +0500

    Update dataschema.js

--- Database/dataschema.js
+++ Database/dataschema.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
 
+const GroupSchema = new mongoose.Schema({
+  id: { type: String,  unique: true ,required: true },
+  antilink: { type: String, default: "false" },
+  nsfw: { type: String, default: "false" },
+  bangroup: { type: String, default: "false" }
+  })
+
 const UserSchema = new mongoose.Schema({
   id: { type: String, unique: true, required: true },
   ban: { type: String, default: "false" },
   name: { type: String },
+  reason: {type: String, default: "no reason" },
   addedMods: { type: String, default: "false" },
 });
 
 const mku = mongoose.model("Mku", UserSchema);
+const mk =mongoose.model("Mk", GroupSchema)
 
-module.exports = { mku };
+module.exports = { mk, mku };
