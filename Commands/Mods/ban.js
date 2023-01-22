7ed4c942d4cb25ab1259dc73bea29663e6fa3a92
kirito2355 <119128285+kirito2355@users.noreply.github.com>
2023-01-22 20:47:02 +0500

    Update ban.js

--- Commands/Mods/ban.js
+++ Commands/Mods/ban.js
@@ -49,6 +49,8 @@ module.exports = {
           var mentionedUser = mentionByTag[0];
         }
       //var mentionedUser = mentionByTag; 
+      let banreason = text.slice(text.indexOf(" ") + 1);
+      if (!banreason) return Miku.sendMessage(m.from, { text: `Please provide the reason for ban.` }, { quoted: m });
       var ownerlist = global.owner;
 
       let userId = (await mentionedUser) || m.msg.contextInfo.participant; 
@@ -56,19 +58,19 @@ module.exports = {
          mku.findOne({id:userId}).then(async (user) => {
             if (!user) {
               if (user.addedMods == "true" || ownerlist.includes(`${mentionedUser.split("@")[0]}`)) return Miku.sendMessage(m.from, { text: `@${mentionedUser.split("@")[0]} is a *Mod* and can't be banned !` , mentions: [mentionedUser]  }, { quoted: m });
-              await mku.create({id:userId, ban: true});
+              await mku.create({id:userId, ban: true, reason: banreason});
               return Miku.sendMessage( 
                 m.from, 
-                { text: `@${mentionedUser.split("@")[0]} has been *Banned* Successfully by *${pushName}* !`, mentions: [mentionedUser] }, 
+                { text: `@${mentionedUser.split("@")[0]} has been *Banned* Successfully by *${pushName}*\n\n *Reason*: ${banreason}`, mentions: [mentionedUser] }, 
                 { quoted: m } 
               );
             }else{
               if (user.addedMods == "true" || ownerlist.includes(`${mentionedUser.split("@")[0]}`)) return Miku.sendMessage(m.from, { text: `@${mentionedUser.split("@")[0]} is a *Mod* and can't be banned !` , mentions: [mentionedUser]  }, { quoted: m });
                 if (user.ban == "true") return Miku.sendMessage(m.from, { text: `@${mentionedUser.split("@")[0]} is already *Banned* !` , mentions: [mentionedUser]  }, { quoted: m });
-                await mku.findOneAndUpdate({ id: userId }, { ban: true }, { new: true });
+                await mku.findOneAndUpdate({ id: userId }, { $set: { ban: true, reason: banreason } }, { new: true });
                 return Miku.sendMessage( 
                   m.from, 
-                  { text: `@${mentionedUser.split("@")[0]} has been *Banned* Successfully by *${pushName}* !` , mentions: [mentionedUser]}, 
+                  { text: `@${mentionedUser.split("@")[0]} has been *Banned* Successfully by *${pushName}*\n\n *Reason*: ${banreason}` , mentions: [mentionedUser]}, 
                   { quoted: m } 
                 );
             }
