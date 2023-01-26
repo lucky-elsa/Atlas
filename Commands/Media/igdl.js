da002323bdeddae81a243cb5ac946ec9a7c8bf90
Fantox <shubhapratimbiswas@gmail.com>
2023-01-27 00:20:05 +0530

    FantoX

--- Commands/Media/igdl.js
+++ Commands/Media/igdl.js
@@ -25,7 +25,7 @@ module.exports = {
             instagramdlv3(`${args.join(" ")}`).then(async (data) => {            
             //var buf = await getBuffer(data.thumbnail)  
 
-            await Miku.sendMessage(m.from, { video: { url: data[0].url }, caption: `_Downloaded by:_ *Miku Nakano*`}, { quoted: m })
+            await Miku.sendMessage(m.from, { video: { url: data[0].url }, caption: `_Downloaded by:_ *${botName}*`}, { quoted: m })
         }).catch((err) => {
             Miku.sendMessage(
                 m.from,
