d514793b34076959d1a3fe28a727f8a36828e55a
Fantox <shubhapratimbiswas@gmail.com>
2023-01-27 00:14:37 +0530

    FantoX

--- Commands/Logo Maker/lion.js
+++ Commands/Logo Maker/lion.js
@@ -11,7 +11,7 @@ module.exports = {
         teks1 = text.split("|")[0]
         teks2 = text.split("|")[1]
         maker.textpro("https://textpro.me/create-lion-logo-mascot-online-938.html", [
-    `${teks1}`,`${teks2}`]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${global.botname}` }, { quoted: m }))
+    `${teks1}`,`${teks2}`]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
--- Commands/Logo Maker/magma.js
+++ Commands/Logo Maker/magma.js
@@ -9,7 +9,7 @@ module.exports = {
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}magma Atlas Bot*`);
         maker.textpro("https://textpro.me/create-a-magma-hot-text-effect-online-1030.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${global.botname}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
