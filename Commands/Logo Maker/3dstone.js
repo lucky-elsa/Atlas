01b5dfecba87a117ec66e96c768c290020cdd9b2
Fantox <shubhapratimbiswas@gmail.com>
2023-01-27 00:07:39 +0530

    FantoX

--- Commands/Logo Maker/3dstone.js
+++ Commands/Logo Maker/3dstone.js
@@ -9,7 +9,7 @@ module.exports = {
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}3dstone Atlas Bot*`);
         maker.textpro("https://textpro.me/3d-stone-cracked-cool-text-effect-1029.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${global.botname}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
