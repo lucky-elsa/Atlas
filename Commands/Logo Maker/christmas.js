d93c5b266bdcb763169a954c03205e2c2331cf42
Fantox <shubhapratimbiswas@gmail.com>
2023-01-27 00:12:33 +0530

    FantoX

--- Commands/Logo Maker/christmas.js
+++ Commands/Logo Maker/christmas.js
@@ -9,7 +9,7 @@ module.exports = {
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}christmas Atlas Bot*`);
         maker.textpro("https://textpro.me/christmas-tree-text-effect-online-free-1057.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${global.botname}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
