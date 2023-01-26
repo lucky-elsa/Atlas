f0d9e4d36ba24878fa117aa874909afe2beadd5d
Fantox <shubhapratimbiswas@gmail.com>
2023-01-27 00:06:48 +0530

    FantoX

--- Commands/Logo Maker/3dneon.js
+++ Commands/Logo Maker/3dneon.js
@@ -9,7 +9,7 @@ module.exports = {
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}3dneon Atlas Bot*`);
         maker.textpro("https://textpro.me/create-3d-neon-light-text-effect-online-1028.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${global.botname}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
