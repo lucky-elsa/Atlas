e8b03fdbf181fe1279706569c39a28c00e4b37a9
kirito2355 <119128285+kirito2355@users.noreply.github.com>
2023-02-04 16:58:35 +0500

    Add files via upload

---
+++ Commands/rpg/mine.js
@@ -0,0 +1,114 @@
+const mongoose = require("mongoose");
+require("../../config.js");
+require("../../Core.js");
+const { shop, player, axe } = require("../../Database/rpgschema.js");
+const eco = require('discord-mongoose-economy')
+const ty = eco.connect('mongodb+srv://fantox001:zjmbvgwr52@cluster0.qh05pl9.mongodb.net/?retryWrites=true&w=majority');
+const fs = require("fs");
+
+
+
+module.exports = {
+    name: "hunt",
+    desc: "Hunt, mine, dig or chop for resources.",
+    alias: ["hunt", "mine", "dig", "chop"],
+    category: "Mine",
+    usage: "hunt/mine/dig/chop",
+    react: "🔨",
+    start: async (Miku, m, { text, prefix }) => {
+        let user = await player.findOne({id:m.sender});
+        if(!user) {
+            return Miku.sendMessage(m.from, { text:` 😕 You don't have an inventory. Use ${prefix}reg-inv to register.` }, { quoted: m });
+        }
+        if(!user.inventory.woodenaxe && !user.inventory.woodpickaxe && !user.inventory.stonepickaxe && !user.inventory.ironpickaxe && !user.inventory.diamondpickaxe){
+            return Miku.sendMessage(m.from, { text:` 😕 Yous don't have any mining equipment. Use ${prefix}buy to purchase one.` }, { quoted: m });
+        }
+        let axeUsed = "woodenaxe";
+            if(user.inventory.woodenaxe){
+            user.inventory.woodenaxe -= 1;
+            let loot = {
+            wood: Math.floor(Math.random() * 4) + 8,
+            stone: Math.floor(Math.random() * 2) + 2,
+            iron: Math.floor(Math.random() * 1) + 1,
+            diamonds: Math.floor(Math.random() * 1)
+            }
+            user.inventory.wood += loot.wood;
+            user.inventory.stone += loot.stone;
+            user.inventory.iron += loot.iron;
+            user.inventory.diamonds += loot.diamonds;
+            await user.save();
+            Miku.sendMessage(m.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n used: ${axeUsed}\n\n *🔮Stone*: ${loot.stone}\n*🔥Wood*: ${loot.wood}\n*🔩Iron*: ${loot.iron}\n*💎Diamonds*: ${loot.diamonds}`}, { quoted: m });
+            }
+            else if(user.inventory.woodpickaxe) {
+            user.inventory.woodpickaxe -= 1;
+            let loot = {
+            wood: Math.floor(Math.random() * 2) + 4,
+            stone: Math.floor(Math.random() * 2) + 2,
+            iron: Math.floor(Math.random() * 1) + 1,
+            diamonds: Math.floor(Math.random() * 1)
+            }
+            axeUsed = "woodpickaxe"
+            user.inventory.wood += loot.wood;
+            user.inventory.stone += loot.stone;
+            user.inventory.iron += loot.iron;
+            user.inventory.diamonds += loot.diamonds;
+            await user.save();
+            Miku.sendMessage(m.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n  used: ${axeUsed}\n\n *🔮Stone*: ${loot.stone}\n*🔥Wood*: ${loot.wood}\n*🔩Iron*: ${loot.iron}\n*💎Diamonds*: ${loot.diamonds}`}, { quoted: m });
+            }
+            else if(user.inventory.stonepickaxe) {
+            user.inventory.stonepickaxe -= 1;
+            let loot = {
+            wood: Math.floor(Math.random() * 1) + 2,
+            stone: Math.floor(Math.random() * 3) + 4,
+            iron: Math.floor(Math.random() * 1) + 1,
+            diamonds: Math.floor(Math.random() * 1)
+            }
+            axeUsed = "stonepickaxe"
+            user.inventory.wood += loot.wood;
+            user.inventory.stone += loot.stone;
+            user.inventory.iron += loot.iron;
+            user.inventory.diamonds += loot.diamonds;
+            await user.save();
+            Miku.sendMessage(m.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n used: ${axeUsed}\n\n 🔮Stone: ${loot.stone}\n🔥Wood: ${loot.wood}\n🔩Iron: ${loot.iron}\n💎Diamonds: ${loot.diamonds}`}, { quoted: m });
+            }
+            else if(user.inventory.ironpickaxe) {
+            user.inventory.ironpickaxe -= 1;
+            let loot = {
+            wood: Math.floor(Math.random() * 1) + 1,
+            stone: Math.floor(Math.random() * 2) + 2,
+            iron: Math.floor(Math.random() * 3) + 4,
+            diamonds: Math.floor(Math.random() * 1)
+            }
+            axeUsed = "ironpickaxe"
+            user.inventory.wood += loot.wood;
+            user.inventory.stone += loot.stone;
+            user.inventory.iron += loot.iron;
+            user.inventory.diamonds += loot.diamonds;
+            await user.save();
+            Miku.sendMessage(m.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n
+            used: ${axeUsed}\n\n 🔮Stone: ${loot.stone}\n🔥Wood: ${loot.wood}\n🔩Iron: ${loot.iron}\n💎Diamonds: ${loot.diamonds}`}, { quoted: m });
+            }
+            else if(user.inventory.diamondpickaxe) {
+                user.inventory.diamondpickaxe -= 1;
+                let loot = {
+                wood: Math.floor(Math.random() * 1),
+                stone: Math.floor(Math.random() * 2) + 2,
+                iron: Math.floor(Math.random() * 2) + 2,
+                diamonds: Math.floor(Math.random() * 3) + 4
+                }
+                axeUsed = "diamondpickaxe"
+                user.inventory.wood += loot.wood;
+                user.inventory.stone += loot.stone;
+                user.inventory.iron += loot.iron;
+                user.inventory.diamonds += loot.diamonds;
+                await user.save();
+                Miku.sendMessage(m.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n used: ${axeUsed}\n\n 🔮Stone: ${loot.stone}\n🔥Wood: ${loot.wood}\n🔩Iron: ${loot.iron}\n💎Diamonds: ${loot.diamonds}`}, { quoted: m });
+                }
+            else {
+            Miku.sendMessage(m.from, { text: `😕 You don't have any mining equipment. Use ${prefix}buy to purchase one. `}, { quoted: m });
+            }
+        }
+    }
+
+
+    
---
+++ Commands/rpg/shop.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+require("../../config.js");
+require("../../Core.js");
+const { player } = require("../../Database/rpgschema.js");
+const eco = require('discord-mongoose-economy')
+const ty = eco.connect('mongodb+srv://fantox001:zjmbvgwr52@cluster0.qh05pl9.mongodb.net/?retryWrites=true&w=majority');
+const fs = require("fs");
+
+
+module.exports = {
+    name: "shop",
+    desc: "To view the list of items available in the shop",
+    alias: ["store"],
+    category: "Shop",
+    usage: "shop",
+    react: "🛍",
+    start: async (Miku, m, { text, prefix, botName}) => {
+        Miku.sendMessage(m.from, { text: `🛍️ 💎 ${global.botName} STORE 💎 🛍️
+
+        👋 Hi there! 🤗 Welcome to the ${global.botName} Store.💼 Here's what you can purchase using Gold 💰:
+        
+#1
+💡 Item: Wooden-Axe
+💰 Cost: 250 Gold
+💻 Usage: ${prefix}buy woodenaxe
+💬 Description: Chop down those trees for some juicy loot! 🌳
+        
+#2
+💡 Item: Stone-Pickaxe
+💰 Cost: 500 Gold
+💻 Usage: ${prefix}buy stonepickaxe
+💬 Description: Mine to your heart's content with this pickaxe! 💎
+        
+#3
+💡 Item: Iron-Pickaxe
+💰 Cost: 2000 Gold
+💻 Usage: ${prefix}buy ironpickaxe
+💬 Description: Mine like a pro with this top-notch pickaxe! 🔨
+        
+#4
+💡 Item: Diamond-Pickaxe
+💰 Cost: 5000 Gold
+💻 Usage: ${prefix}buy diamondpickaxe
+💬 Description: The ultimate mining experience awaits! 💎💰` }, { quoted: m });
+    }
+}
