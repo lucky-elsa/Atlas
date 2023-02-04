4b6abb9bff9dd92d986762eb26c17bb599b78fde
kirito2355 <119128285+kirito2355@users.noreply.github.com>
2023-02-04 20:43:55 +0500

    Update mine.js

--- Commands/rpg/mine.js
+++ Commands/rpg/mine.js
@@ -9,106 +9,92 @@ const fs = require("fs");
 
 
 module.exports = {
-    name: "hunt",
+    name: "hunt2",
     desc: "Hunt, mine, dig or chop for resources.",
-    alias: ["hunt", "mine", "dig", "chop"],
+    alias: ["hunt3"],
     category: "Mine",
-    usage: "hunt/mine/dig/chop",
+    usage: "hunt/mine/dig/chop [axe]",
     react: "🔨",
-    start: async (Miku, m, { text, prefix }) => {
-        let user = await player.findOne({id:m.sender});
-        if(!user) {
-            return Miku.sendMessage(m.from, { text:` 😕 You don't have an inventory. Use ${prefix}reg-inv to register.` }, { quoted: m });
-        }
-        if(!user.inventory.woodenaxe && !user.inventory.woodpickaxe && !user.inventory.stonepickaxe && !user.inventory.ironpickaxe && !user.inventory.diamondpickaxe){
-            return Miku.sendMessage(m.from, { text:` 😕 Yous don't have any mining equipment. Use ${prefix}buy to purchase one.` }, { quoted: m });
-        }
-        let axeUsed = "woodenaxe";
-            if(user.inventory.woodenaxe){
-            user.inventory.woodenaxe -= 1;
-            let loot = {
+    start: async (Miku, message, { text, prefix, args }) => {
+      const user = await player.findOne({ id: message.sender });
+      if (!user) {
+        return Miku.sendMessage(message.from, { text:` 😕 You don't have an inventory. Use ${prefix}reg-inv to register.` }, { quoted: message });
+      }
+    
+      const axeUsed = args[0]
+      if (!axeUsed) {
+        return Miku.sendMessage(message.from, { text: `😕 You need to specify which axe to use (woodenAxe, woodPickaxe, stonePickaxe, ironPickaxe, diamondPickaxe).` }, { quoted: message });
+      }
+    
+      if (!user.inventory[axeUsed]) {
+        return Miku.sendMessage(message.from, { text:` 😕 You don't have a ${axeUsed}. Use ${prefix}buy to purchase one.` }, { quoted: message });
+      }
+      let loot;
+      switch (axeUsed) {
+        case "woodenaxe":
+          user.inventory.woodenaxe -= 1;
+           loot = {
             wood: Math.floor(Math.random() * 4) + 8,
             stone: Math.floor(Math.random() * 2) + 2,
             iron: Math.floor(Math.random() * 1) + 1,
             diamonds: Math.floor(Math.random() * 1)
-            }
-            user.inventory.wood += loot.wood;
-            user.inventory.stone += loot.stone;
-            user.inventory.iron += loot.iron;
-            user.inventory.diamonds += loot.diamonds;
-            await user.save();
-            Miku.sendMessage(m.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n used: ${axeUsed}\n\n *🔮Stone*: ${loot.stone}\n*🔥Wood*: ${loot.wood}\n*🔩Iron*: ${loot.iron}\n*💎Diamonds*: ${loot.diamonds}`}, { quoted: m });
-            }
-            else if(user.inventory.woodpickaxe) {
-            user.inventory.woodpickaxe -= 1;
-            let loot = {
-            wood: Math.floor(Math.random() * 2) + 4,
-            stone: Math.floor(Math.random() * 2) + 2,
-            iron: Math.floor(Math.random() * 1) + 1,
-            diamonds: Math.floor(Math.random() * 1)
-            }
-            axeUsed = "woodpickaxe"
-            user.inventory.wood += loot.wood;
-            user.inventory.stone += loot.stone;
-            user.inventory.iron += loot.iron;
-            user.inventory.diamonds += loot.diamonds;
-            await user.save();
-            Miku.sendMessage(m.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n  used: ${axeUsed}\n\n *🔮Stone*: ${loot.stone}\n*🔥Wood*: ${loot.wood}\n*🔩Iron*: ${loot.iron}\n*💎Diamonds*: ${loot.diamonds}`}, { quoted: m });
-            }
-            else if(user.inventory.stonepickaxe) {
+          };
+          user.inventory.wood += loot.wood;
+          user.inventory.stone += loot.stone;
+          user.inventory.iron += loot.iron;
+          user.inventory.diamonds += loot.diamonds;
+          await user.save();
+          Miku.sendMessage(message.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n used: ${axeUsed}\n\n *🔮Stone*: ${loot.stone}\n*🔥Wood*: ${loot.wood}\n*🔩Iron*: ${loot.iron}\n*💎Diamonds*: ${loot.diamonds}`}, { quoted: message });
+          break;
+            case "stonepickaxe":
             user.inventory.stonepickaxe -= 1;
-            let loot = {
-            wood: Math.floor(Math.random() * 1) + 2,
-            stone: Math.floor(Math.random() * 3) + 4,
-            iron: Math.floor(Math.random() * 1) + 1,
-            diamonds: Math.floor(Math.random() * 1)
-            }
-            axeUsed = "stonepickaxe"
+             loot = {
+            wood: Math.floor(Math.random() * 2) + 2,
+            stone: Math.floor(Math.random() * 2) + 4,
+            iron: Math.floor(Math.random() * 1) + 2,
+            diamonds: Math.floor(Math.random() * 1) + 1
+            };
             user.inventory.wood += loot.wood;
             user.inventory.stone += loot.stone;
             user.inventory.iron += loot.iron;
             user.inventory.diamonds += loot.diamonds;
             await user.save();
-            Miku.sendMessage(m.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n used: ${axeUsed}\n\n 🔮Stone: ${loot.stone}\n🔥Wood: ${loot.wood}\n🔩Iron: ${loot.iron}\n💎Diamonds: ${loot.diamonds}`}, { quoted: m });
-            }
-            else if(user.inventory.ironpickaxe) {
-            user.inventory.ironpickaxe -= 1;
-            let loot = {
-            wood: Math.floor(Math.random() * 1) + 1,
-            stone: Math.floor(Math.random() * 2) + 2,
-            iron: Math.floor(Math.random() * 3) + 4,
-            diamonds: Math.floor(Math.random() * 1)
-            }
-            axeUsed = "ironpickaxe"
-            user.inventory.wood += loot.wood;
-            user.inventory.stone += loot.stone;
-            user.inventory.iron += loot.iron;
-            user.inventory.diamonds += loot.diamonds;
-            await user.save();
-            Miku.sendMessage(m.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n
-            used: ${axeUsed}\n\n 🔮Stone: ${loot.stone}\n🔥Wood: ${loot.wood}\n🔩Iron: ${loot.iron}\n💎Diamonds: ${loot.diamonds}`}, { quoted: m });
-            }
-            else if(user.inventory.diamondpickaxe) {
-                user.inventory.diamondpickaxe -= 1;
-                let loot = {
-                wood: Math.floor(Math.random() * 1),
-                stone: Math.floor(Math.random() * 2) + 2,
-                iron: Math.floor(Math.random() * 2) + 2,
-                diamonds: Math.floor(Math.random() * 1001) + 7000
-                }
-                axeUsed = "diamondpickaxe"
+            Miku.sendMessage(message.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n used: ${axeUsed}\n\n *🔮Stone*: ${loot.stone}\n*🔥Wood*: ${loot.wood}\n*🔩Iron*: ${loot.iron}\n*💎Diamonds*: ${loot.diamonds}`}, { quoted: message });
+            break;
+            case "ironpickaxe":
+                user.inventory.ironpickaxe -= 1;
+                 loot = {
+                wood: Math.floor(Math.random() * 1) + 1,
+                stone: Math.floor(Math.random() * 2) + 4,
+                iron: Math.floor(Math.random() * 1) + 4,
+                diamonds: Math.floor(Math.random() * 1) + 2
+                };
                 user.inventory.wood += loot.wood;
                 user.inventory.stone += loot.stone;
                 user.inventory.iron += loot.iron;
                 user.inventory.diamonds += loot.diamonds;
                 await user.save();
-                Miku.sendMessage(m.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n used: ${axeUsed}\n\n 🔮Stone: ${loot.stone}\n🔥Wood: ${loot.wood}\n🔩Iron: ${loot.iron}\n💎Diamonds: ${loot.diamonds}`}, { quoted: m });
+                Miku.sendMessage(message.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n used: ${axeUsed}\n\n *🔮Stone*: ${loot.stone}\n*🔥Wood*: ${loot.wood}\n*🔩Iron*: ${loot.iron}\n*💎Diamonds*: ${loot.diamonds}`}, { quoted: message });
+                break;
+                case "diamondpickaxe":
+                user.inventory.diamondpickaxe -= 1;
+                loot = {
+                wood: Math.floor(Math.random() * 1),
+                stone: Math.floor(Math.random() * 2) + 4,
+                iron: Math.floor(Math.random() * 1) + 4,
+                diamonds: Math.floor(Math.random() * 1001) + 7000
+                };
+                user.inventory.wood += loot.wood;
+                user.inventory.stone += loot.stone;
+                user.inventory.iron += loot.iron;
+                user.inventory.diamonds += loot.diamonds;
+                await user.save();
+                Miku.sendMessage(message.from, { text: `[ 🐺MINE RESULT🐺 ]\n\n used: ${axeUsed}\n\n *🔮Stone*: ${loot.stone}\n*🔥Wood*: ${loot.wood}\n*🔩Iron*: ${loot.iron}\n*💎Diamonds*: ${loot.diamonds}`}, { quoted: message });
+                break;
+                default:
+                Miku.sendMessage(message.from, { text: `😕 Invalid axe specified, valid axes are (woodenAxe, woodPickaxe, stonePickaxe, ironPickaxe, diamondPickaxe). `}, { quoted: message });
+                break;
                 }
-            else {
-            Miku.sendMessage(m.from, { text: `😕 You don't have any mining equipment. Use ${prefix}buy to purchase one. `}, { quoted: m });
-            }
-        }
-    }
-
-
+                }
+                };
     
