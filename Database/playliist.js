4875248addf5036107c4690f366626afbe4c0446
kirito2355 <119128285+kirito2355@users.noreply.github.com>
2023-02-08 22:15:12 +0500

    Update playliist.js

--- Database/playliist.js
+++ Database/playliist.js
@@ -1,25 +1,14 @@
 const mongoose = require("mongoose");
 
-const songSchema = new mongoose.Schema({
-    url: {
-        type: String,
-        required: true
-    }
-});
-
 const playlistSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    owner: {
-        type: String,
-        required: true
-    },
-    date: { type: Date },
-    songs: [songSchema]
-});
+    name: { type: String, required: true },
+    owner: { type: String, required: true },
+    songs: [{
+      title: { type: String },
+      url: { type: String },
+      savedAt: { type: Date, default: Date.now }
+    }]
+  });
 
 const playlist = mongoose.model("Playlist", playlistSchema);
 module.exports = { playlist };
