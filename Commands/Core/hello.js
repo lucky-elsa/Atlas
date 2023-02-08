8d5bc8056bd8b0cdd598ba1d70b3930390eab5da
Fantox <shubhapratimbiswas@gmail.com>
2023-02-08 19:43:55 +0530

    Update hello.js

--- Commands/Core/hello.js
+++ Commands/Core/hello.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "hi",
     alias: ["hello"],
     desc: "Say hello to bot.",
-    react: "🍁",
+    react: "🧣",
     category: "Core",
     start: async(Miku, m,{pushName,prefix}) => {
         await Miku.sendMessage(m.from,{text:`Konichiwa *${pushName}* senpai, I am *${botName}* bot. type *${prefix}help* to get my full command list.`},{quoted:m})
