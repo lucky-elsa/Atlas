98e6e141c144d93494e488010eccd595e8b7ca58
Fantox <shubhapratimbiswas@gmail.com>
2023-02-08 17:11:37 +0530

    Create script.js

---
+++ Commands/Core/script.js
@@ -0,0 +1,17 @@
+const axios = require('axios')
+const fs = require('fs')
+
+module.exports = {
+    name: "script",
+    alias: ["repo","sc","sourcecode"],
+    desc: "Say hello to bot.",
+    react: "🍁",
+    category: "Core",
+    start: async(Miku, m,{pushName,prefix}) => {
+        let picURL = fs.readFileSync('./Page/BG.jpg')
+        let repoInfo = await axios.get('https://api.github.com/repos/FantoX001/Atlas-MD')
+        let repo = repoInfo.data
+        let txt = `      🧣 *${botName}'s Script* 🧣\n\n*🎀 Total Forks:* ${repo.forks_count}\n*⭐ Total Stars:* ${repo.stargazers_count}\n*📜 License:* ${repo.license.name}\n*📁 Repo Size:* ${(repo.size/1024).toFixed(2)} MB\n*📅 Last Updated:* ${repo.updated_at}\n\n*🔗 Repo Link:* ${repo.html_url}\n\n❝ Dont forget to give a Star ⭐ to the repo. It's made with restless harkwork by *Team ATLAS*. ❞\n\n*©️ Team ATLAS- 2023*`
+        await Miku.sendMessage(m.from,{image:picURL, caption:txt},{quoted:m});
+    }
+}
