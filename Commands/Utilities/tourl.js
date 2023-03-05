eaea499d0b115ad7720106be5f182215ae061692
Fantox <96835724+FantoX001@users.noreply.github.com>
2023-03-06 02:17:01 +0530

    V-2.1 Release

--- Commands/Utilities/tourl.js
+++ Commands/Utilities/tourl.js
@@ -1,24 +1,28 @@
-let { UploadFileUgu, webp2mp4File, TelegraPh } = require('../../lib/uploader')
-const fs = require('fs');
-const util = require('util')
+let { TelegraPh } = require("../../lib/uploader");
+const fs = require("fs");
+const util = require("util");
 
 module.exports = {
-    name: "tourl",
-    alias: ["makeurl"],
-    desc: "To make a url from image/video/gif",
-    category: "Utilities",
-    usage: "sticker <reply to image>",
-    react: "🍁",
-    start: async (Miku, m, { text, prefix,quoted,pushName,mime,body }) => {
-        let media = await Miku.downloadAndSaveMediaMessage(quoted);
-        let m2 = await quoted.download()
-        if (/image/.test(mime)) {
-            let anu = await TelegraPh(media)
-            //m.reply(`*Generated Image Link:* \n\n${util.format(anu)}\n`)
-            await Miku.sendMessage(m.from, { image: m2, caption:`*Generated Image Link:* \n\n${util.format(anu)}\n` }, { quoted: m });
-            } else if (!/image/.test(mime)) {
-            m.reply(`Plese provide an image to generate a link!`)
-            }
-            await fs.unlinkSync(media)
-            }
-        }
+  name: "tourl",
+  alias: ["makeurl"],
+  desc: "To make a url from image/video/gif",
+  category: "Utilities",
+  usage: "sticker <reply to image>",
+  react: "🍁",
+  start: async (Miku, m, { quoted, mime }) => {
+    let media = await Miku.downloadAndSaveMediaMessage(quoted);
+    if (/image/.test(mime)) {
+      let anu = await TelegraPh(media);
+      m.reply(`*Generated Image URL:* \n\n${util.format(anu)}\n`);
+    } else if (/video/.test(mime)) {
+      if (media.size > 5 * 1024 * 1024) {
+        m.reply(`*Your video size is too big!*\n\n*Max video size:* 5MB`);
+      }
+      let anu = await TelegraPh(media);
+      m.reply(`*Generated Video URL:* \n\n${util.format(anu)}\n`);
+    } else {
+      m.reply(`Plese provide an image to generate a link!`);
+    }
+    await fs.unlinkSync(media);
+  },
+};
